554dedf2f4fafd759cd9afdc4f6b664fe90a3b40
Felipe Bedoya <bedoyafelipe743@gmail.com>
2020-05-02 14:44:04 +0200

    Añadiendo estilo al componente

--- src/components/Resumen.js
+++ src/components/Resumen.js
@@ -1,4 +1,13 @@
 import React, { Fragment } from 'react';
+import styled from '@emotion/styled';
+
+const ContenedorResumen = styled.div`
+    padding: 1rem;
+    text-align: center;
+    background-color: black;
+    color: #FFF;
+    margin-top: 1rem;
+`;
 
 const Resumen = ({datos}) => {
 
@@ -8,14 +17,14 @@ const Resumen = ({datos}) => {
     if (marca === '' || year === '' || plan === '' ) return null;
 
     return ( 
-        <Fragment>
+        <ContenedorResumen>
             <h2>Resumen de la cotización</h2>
             <ul>
                 <li>Marca:</li>
                 <li>Plan:</li>
                 <li>Año del auto:</li>
             </ul>
-        </Fragment>
+        </ContenedorResumen>
      );
 }
  
