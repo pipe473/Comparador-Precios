2415551707e4ac03a7514df1faf6b3ca1793e65f
Felipe Bedoya <bedoyafelipe743@gmail.com>
2020-05-03 00:32:46 +0200

    Dandole estilo al mensaje de resultado y mostrando el resultado

--- src/components/Resultado.js
+++ src/components/Resultado.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import styled from '@emotion/styled';
+
+const Mensaje = styled.p`
+    background-color: black;
+    color:white;
+    margin-top: 2rem;
+    padding: 1rem;
+    text-align: center;
+`;
 
 const Resultado = ({cotizacion}) => {
 
 
     return ( 
-        (cotizacion === 0) ? <p>Elige marca, año y tipo de seguro</p> : cotizacion
+        (cotizacion === 0) 
+                ? <Mensaje>Elige marca, año y tipo de seguro</Mensaje> 
+                : <p>El total es: {cotizacion}€</p>
+                
+                
      );
 }
  
